9732e0f124d03e8367e6a60e63334e5e904a94eb
Alejandro <lealalejandro422@gmail.com>
2024-11-16 14:16:59 -0500

    Added a pinterest-like workflow

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,20 @@
 document.getElementById('analisis').addEventListener("click", () => {alert("Button clicked");});
+
+//add a box
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('addBox').addEventListener('click',function(){
+        const content = document.getElementById("boxContent").value;
+        if (content.trim() === ''){
+            alert('añadir contenido');
+            return;
+        }
+
+        const newBox = document.createElement('div');
+        newBox.classList.add('grid-box');
+        newBox.textContent = content;
+
+        document.getElementById("mainGrid").appendChild(newBox);
+
+        document.getElementById('boxContent').value = '';
+    });
+});
