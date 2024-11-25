fbd6cd365c0e35672fe9da932605e9a5e1d28853
ALELOY257 <lealalejandro422@gmail.com>
2024-11-25 14:50:31 -0500

    added virustotal support and aesthetic changes

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,36 @@
 
-  
+//listen for messages form the popup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log('message received', request);
+    if (request.action === "getVirusTotalAnalysis"){
+        const url = request.url;
+        const apiKey = '5bb389eb66d9e09c19a952a49f0cc6cad7f8857818ac7a0154aadcb33b2bc955'
+
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              'x-apikey': apiKey
+            }
+          };
+        
+        const fetchVirusTotalAnalysis = async () => {
+            try {
+                console.log('Making API request...');
+                const res = await fetch(url, options);
+                
+                const data = await res.json();
+                console.log('API response received:', data);
+                sendResponse({ result: data });
+            } catch (err) {
+                console.error('Error during fetch:', err);
+                sendResponse({ err: 'Failed to fetch data from VT' });
+            }
+        };
+
+        fetchVirusTotalAnalysis();
+            
+            return true;
+    }
+})
   
--- extension/popup.js
+++ extension/popup.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function(){
         const newBox = document.createElement('div');
         newBox.classList.add('grid-box');
         newBox.textContent = content;
+        console.log("box created")
 
         document.getElementById("mainGrid").appendChild(newBox);
 
@@ -37,6 +38,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const urlInputContainter = document.getElementById('urlInputContainer');
     const submitUrlButton = document.getElementById('submitUrl');
     const urlInputField = document.getElementById('urlInput');
+    const okButton = document.getElementById('aceptar')
+    const container = document.getElementById('vtdataContainer');
 
     //show the box
     openUrlInputButton.addEventListener('click', function(){
@@ -46,6 +49,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
     //handling
     submitUrlButton.addEventListener('click', function(){
+        container.classList.toggle('ocultar');
+
         const url = urlInputField.value.trim()
 
         if(url==''){
@@ -57,8 +62,51 @@ document.addEventListener('DOMContentLoaded', function(){
         
         let urlId = generateUrlUnicodeSafe(url);
         
+        let finalUrl = "https://www.virustotal.com/api/v3/urls/"+urlId;
 
-
+        let rawData;
+        
+        //Call virustotal API from background
+        chrome.runtime.sendMessage(
+            {action: "getVirusTotalAnalysis", url: finalUrl},
+            (response) =>{
+                if(!response) {
+                    console.error("No response from background script");
+                    document.getElementById('result').innerText="No response from bg script";
+                } 
+                
+                if (response.err){
+                    console.error(response.err);
+                    document.getElementById('result').innerText = "Error: "+ response.err;
+                }
+                else {
+                    const analysisResult = response.result;
+                    console.log(analysisResult)
+                    //data depuration
+                    const threats = analysisResult.data.attributes.threat_names;
+                    const analysisStats = analysisResult.data.attributes.last_analysis_stats;
+                    const maliciousEngines = Object.entries(analysisResult.data.attributes.last_analysis_results).filter(([engine, result]) => result.category ==="malicious");
+                    const trackers = Object.keys(analysisResult.data.attributes.trackers);
+                    //show in html
+                    let resultHtml =`
+                    
+                                        <h2>Analisis</h2>
+                                        <p><strong>Amenazas:</strong> ${threats.join(", ") || "Ninguna"}</p>
+                                        <p><strong>Estadisticas del analisis:</strong> Inofensivas: ${analysisStats.harmless}, 
+                                        Maliciosas: ${analysisStats.malicious}, Sospechosas: ${analysisStats.suspicious}, 
+                                        No detectadas: ${analysisStats.undetected}</p>
+                                        <p><strong>Motores maliciosos:</strong> ${maliciousEngines.join(", ") || "None"}</p>
+                                        <p><strong>Trackers:</strong> ${trackers.join(", ") || "None"}</p>
+                                    `;
+                    resultHtml = resultHtml.replace(/\n/g, "<br>"); 
+                    document.getElementById('data').innerHTML = JSON.stringify(resultHtml, null, 2);
+                }
+            }
+        )
+        
+        okButton.addEventListener('click', function(){
+            container.classList.toggle('ocultar');
+        })
         urlInputField.value='';
         urlInputContainter.classList.toggle('hidden');
     });
